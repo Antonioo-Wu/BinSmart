872f65d53c0452f7d6bc4334eab27b7a639b1524
Antonioo-Wu <anwu@uade.edu.ar>
2025-10-15 23:31:54 -0300

    Update api.js

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -5,7 +5,7 @@ const baseURL = API_URL || 'http://localhost:3000/api';
 
 const api = axios.create({
   baseURL: API_URL,
-  //timeout: 10000,
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
